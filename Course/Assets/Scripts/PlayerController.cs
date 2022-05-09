4c99ce56d6f115fac5fd5d17c08f54d97a7f89a2
Georg Becker <Georg.Becker@gmx.at>
2022-05-09 18:39:58 +0200

    Adds ground collision check

--- Course/Assets/Scripts/PlayerController.cs
+++ Course/Assets/Scripts/PlayerController.cs
@@ -7,6 +7,10 @@ public class PlayerController : MonoBehaviour
     public float moveSpeed;
     public float jumpForce;
     public Rigidbody2D rigidBody;
+    public Transform groundPoint;
+    public LayerMask whatIsGround;
+    
+    private bool isOnGround;
     
     // Start is called before the first frame update
     void Start()
@@ -18,10 +22,12 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         rigidBody.velocity = new Vector2(moveSpeed * Input.GetAxis("Horizontal"), rigidBody.velocity.y);
+
+        isOnGround = Physics2D.OverlapCircle(groundPoint.position, 0.1f, whatIsGround);
         
-        if(Input.GetButtonDown("Jump"))
+        if(isOnGround && Input.GetButtonDown("Jump"))
         {
-            rigidBody.velocity = new Vector2(rigidBody.velocity.x, rigidBody.velocity.y + jumpForce);
+            rigidBody.velocity = new Vector2(rigidBody.velocity.x,jumpForce);
         }
     }
 }
