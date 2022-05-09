9f79abdb18af43d50e450639175a5a50e4a119ad
Georg Becker <Georg.Becker@gmx.at>
2022-05-09 20:26:37 +0200

    Adds paralax background

--- Course/Assets/Scripts/CameraController.cs
+++ Course/Assets/Scripts/CameraController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,6 +6,8 @@ using UnityEngine;
 public class CameraController : MonoBehaviour
 {
     public Transform target;
+
+    public Transform farBackground, middleBackground;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,6 +17,10 @@ public class CameraController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Vector3 lastPosition = new Vector3(transform.position.x, transform.position.y, transform.position.z);
         transform.position = new Vector3(target.position.x, transform.position.y, transform.position.z);
+        
+        farBackground.position = new Vector3(farBackground.position.x + (transform.position.x - lastPosition.x), farBackground.position.y, farBackground.position.z);
+        middleBackground.position = new Vector3(middleBackground.position.x + (transform.position.x - lastPosition.x) * 0.6f, middleBackground.position.y, middleBackground.position.z);
     }
 }
