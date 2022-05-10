88f71e173ce834f5933585c66de310a14174eedd
Georg Becker <Georg.Becker@gmx.at>
2022-05-10 10:16:07 +0200

    Add left and right border

--- Course/Assets/Scripts/CameraController.cs
+++ Course/Assets/Scripts/CameraController.cs
@@ -7,6 +7,7 @@ public class CameraController : MonoBehaviour
 {
     public Transform target;
     public float minHeight, maxHeight;
+    public float minX, maxX;
 
     public Transform farBackground, middleBackground;
     // Start is called before the first frame update
@@ -19,7 +20,7 @@ public class CameraController : MonoBehaviour
     void Update()
     {
         Vector3 lastPosition = new Vector3(transform.position.x, transform.position.y, transform.position.z);
-        transform.position = new Vector3(target.position.x, Mathf.Clamp(target.position.y,minHeight,maxHeight), transform.position.z);
+        transform.position = new Vector3(Mathf.Clamp(target.position.x,minX,maxX), Mathf.Clamp(target.position.y,minHeight,maxHeight), transform.position.z);
         
         farBackground.position = new Vector3(farBackground.position.x + (transform.position.x - lastPosition.x), farBackground.position.y + (transform.position.y - lastPosition.y) * 0.9f, farBackground.position.z);
         middleBackground.position = new Vector3(middleBackground.position.x + (transform.position.x - lastPosition.x) * 0.6f, middleBackground.position.y + (transform.position.y - lastPosition.y) * 0.7f, middleBackground.position.z);
